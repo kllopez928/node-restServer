85471917f0b8e4bc8d8d7761e9bd659be9f2cbe5
kevin <kllopez928@gmail.com>
2020-05-27 17:40:26 -0500

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,26 @@
 // Puerto
 // =================
 
-
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =================
+// Entorno
+// =================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =================
+//  Base de datos
+// =================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://kllopez928:e91Gu8Q1ZPfwv5E0@cluster0-8e0sh.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
